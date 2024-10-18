2614e203926d690de0f1fe4526129a53b3edd2d1
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-10-19 00:01:13 +0530

    code cleaning

--- src/db/index.js
+++ src/db/index.js
@@ -5,11 +5,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        
-        console.log(`\n MongoDB connected!! DB Host: ${connectionInstance.connection.host}`);
+        console.log(`\n MongoDB connected!!`);
         
     } catch (error) {
-        console.log(process.env.MONGODB_URI);
         console.log('MONGODB connection error', error);
         process.exit(1)
     }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,3 @@ import connectDB from "./db/index.js";
 
 dotenv.config()
 connectDB();
-
-// ;(async () => {
-//     try{
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error", (err)=>{
-//             console.log('err');
-//             throw err;
-//         })
-//         app.listen(process.env.PORT, ()=>{
-//             console.log('App Listening on port', process.env.PORT);
-            
-//         })
-//     }catch(err){
-//         console.log("ERROR: ",err);
-//         throw err;
-//     }
-// })()
